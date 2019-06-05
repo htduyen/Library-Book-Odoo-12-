a5a31a6ce8c38ddc90ed8c50b9ce5a0632b4580d
htduyen <thanhduyeny1@gmail.com>
2019-06-05 13:53:55 +0700

    Complete chapter 5

---
+++ library_member/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Library Members',
+    'description': 'Manage people who will be able to borrow books.',
+    'author': 'Daniel Reis',
+    'data':  [
+        'views/book_view.xml',
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/member_view.xml',
+        'views/library_menu.xml',
+        'views/book_list_template.xml',
+    ], 
+     'demo': [
+        'data/res.partner.csv',
+        'data/library.book.csv',
+        'data/book_demo.xml',
+    ],  
+    'depends': ['library_app', 'mail'],
+    'application': False,
+}
